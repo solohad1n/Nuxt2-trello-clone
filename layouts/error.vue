11410dadba2da1b7d55cc9ff303d9f38156df05a
solohad1n <fox663399@gmail.com>
2022-09-19 16:02:00 +0600

    Added a village with boards. Added pack with time

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,50 @@
 <template>
-  <v-app>
-    <h1>Ошибка!</h1>
+  <v-app dark>
+    <div class="error">
+      <h1 v-if="error.statusCode === 404">
+        {{ pageNotFound }}
+      </h1>
+      <h1 v-else>
+        {{ otherError }}
+      </h1>
+      <NuxtLink to="/"> Домашняя страница! </NuxtLink>
+    </div>
   </v-app>
 </template>
 
 <script>
-export default {}
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      pageNotFound: '404 Не найдено! ->->->',
+      otherError: 'Произошла ошибка! ->->->',
+    }
+  },
+  head() {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title,
+    }
+  },
+}
 </script>
 
 <style scoped>
+h1 {
+  font-size: 20px;
+  margin-right: 20px;
+}
+.error {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
